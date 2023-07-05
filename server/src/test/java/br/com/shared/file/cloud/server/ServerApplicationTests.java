6589a0b1b4095b2cf6ef2993cfdc473a10779a1b
jonassantos1000 <jonasvale3@hotmail.com>
2023-07-04 21:18:08 -0300

    feat: implementado servidor e api gateway para gerenciar os microserviços

---
+++ server/src/test/java/br/com/shared/file/cloud/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.shared.file.cloud.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
