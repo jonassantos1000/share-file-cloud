ccb49e2b4f3818bdaa3c53f6b75922b5cf4149cb
jonassantos1000 <jonasvale3@hotmail.com>
2023-07-04 23:16:22 -0300

    feat: implementado entidade para representar os uploads

--- api/src/main/java/br/com/shared/file/cloud/api/domain/Upload.java
+++ api/src/main/java/br/com/shared/file/cloud/api/domain/Upload.java
@@ -1,5 +1,8 @@
 package br.com.shared.file.cloud.api.domain;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,9 +10,80 @@ import javax.persistence.Id;
 
 @Entity
 public class Upload {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String fileName;
+	private String base64;
+	private LocalDateTime createDate;
+	private String storageId;
+
+	public Upload() {
+	}
+
+	public Upload(String fileName, String base64, String storageId) {
+		this.fileName = fileName;
+		this.base64 = base64;
+		this.storageId = storageId;
+		this.createDate = LocalDateTime.now();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getBase64() {
+		return base64;
+	}
+
+	public void setBase64(String base64) {
+		this.base64 = base64;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getStorageId() {
+		return storageId;
+	}
+
+	public void setStorageId(String storageId) {
+		this.storageId = storageId;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, storageId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Upload other = (Upload) obj;
+		return Objects.equals(id, other.id) && Objects.equals(storageId, other.storageId);
+	}
 
 }
